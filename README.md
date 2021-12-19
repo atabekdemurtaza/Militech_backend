e759632eb15ef63076672b4bea640c16d8f216a4
atabekdemurtaza <mychannel21war@gmail.com>
2021-12-20 02:36:33 +0500

    Second

---
+++ README.md
@@ -0,0 +1,18 @@
+#Install virtual environment
+	
+		py -m venv myenv 
+
+#Install all additional packages from requirements.txt. You should write:
+	
+		pip install -r requirements.txt 
+
+#Done! You can turn on server 
+
+		py manage.py runserver
+
+#In order to peruse you may write below code in yout CLI 
+	
+
+		curl http://127.0.0.1:8000/
+
+#Enjoy
--- post/views.py
+++ post/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from post.models import Post 
+from post.serializers import PostSerializer
+from rest_framework import generics 
 
-# Create your views here.
+class ListPost(generics.ListAPIView):
+
+	queryset = Post.objects.all()
+	serializer_class = PostSerializer 
+
+class DetailPost(generics.RetrieveAPIView):
+
+	queryset = Post.objects.all()
+	serializer_class = PostSerializer
+
